999dc2d5774a458d4f73a6faf369d3cd26d88ae0
Paige Vannelli <paigevannelli@gmail.com>
2021-05-13 16:26:24 -0600

    Pass down header and inputs to Form and create Form prop interfaces
    Co-authored-by:AlexT ahthomps1@gmail.com
    Co-authored-by:PaigeV paigevannelli@gmail.com
    Co-authored-by:StevenM itsnameissteven@gmail.com”

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from '../Form/Form'
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [name, setName] = useState('')
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+
   return (
     <div className="App">
+      <Form 
+        header="name"
+        inputs={[{property: name, method: setName, placeholder: 'name'}, 
+        {property: username, method: setUsername, placeholder: 'username'}, 
+        {property: email, method: setEmail, placeholder: 'email'}]} 
+      />
     </div>
   );
 }
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
-const Form = () => {
+interface FormProps {
+  header: string
+  inputs: Array<Inputs>
+}
+
+interface Inputs {
+  property: string
+  method: React.Dispatch<React.SetStateAction<string>>
+  placeholder: string
+}
+
+const Form: React.FC<FormProps> = ({ header, inputs}): JSX.Element => {
   return (
     <div>
 
