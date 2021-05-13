77830ccb383d1ff5c22ca8125adec5c31e0df875
Paige Vannelli <paigevannelli@gmail.com>
2021-05-13 16:33:51 -0600

    Map over inputs and dipslay inputs on form
    Co-authored-by:AlexT ahthomps1@gmail.com
    Co-authored-by:PaigeV paigevannelli@gmail.com
    Co-authored-by:StevenM itsnameissteven@gmail.com

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -12,12 +12,23 @@ interface Inputs {
 }
 
 const Form: React.FC<FormProps> = ({ header, inputs}): JSX.Element => {
+  const allInputs = inputs.map((input, index) => {
+    return (
+      <input
+        key={index}
+        value={input.property}
+        placeholder={input.placeholder}
+        onChange={event => input.method(event.target.value)}
+      />
+    )
+  })
+
   return (
     <div>
-
+      <h1>{ header }</h1>
+      { allInputs }
     </div>
   )
-
 }
 
 export default Form 
