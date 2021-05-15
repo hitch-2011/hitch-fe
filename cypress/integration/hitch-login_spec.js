451534a7155b91f6ce8fe33bdf1f8d7d81f4240a
Alex Thompson <ahthomps1@gmail.com>
2021-05-15 16:54:48 -0600

    Add a beforeEach hook to login test

--- cypress/integration/hitch-login_spec.js
+++ cypress/integration/hitch-login_spec.js
@@ -1,5 +1,9 @@
 describe('Login User Flow', () => {
 
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
   it('should be a passing test', () => {
     expect(true).to.equal(true);
   })
