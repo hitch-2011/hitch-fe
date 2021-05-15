b392b00a21a48c15f2b6bdea6db1d6eae83a115f
Alex Thompson <ahthomps1@gmail.com>
2021-05-15 17:03:22 -0600

    Add test for user name info on form

--- cypress/integration/hitch-login_spec.js
+++ cypress/integration/hitch-login_spec.js
@@ -1,4 +1,4 @@
-describe('Login User Flow', () => {
+describe('Sign up user flow', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
@@ -11,4 +11,26 @@ describe('Login User Flow', () => {
   it('should have button to sign up', () => {
     cy.get('button').contains('Sign up now');
   });
+
+  it('should be able to click \'sign up now\' button', () => {
+    cy.get('button').click();
+    cy.url().should('include', 'http://localhost:3000/register')
+  });
+});
+
+describe.only('User name input', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/register');
+  });
+
+  it('should have an app title', () => {
+    cy.get('h1').contains('HITCH');
+  });
+
+  it('should have a form title ', () => {
+    cy.get('.register-form__header').contains('Name');
+  });
+
+
 })
