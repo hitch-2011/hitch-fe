60c439aa3ca3a0c91e61075b60b85ccc867831af
Paige Vannelli <paigevannelli@gmail.com>
2021-05-16 14:13:23 -0600

    Set up route for DetailedRoute component in App and pass down id

--- src/components/DetailedRoute/DetailedRoute.tsx
+++ src/components/DetailedRoute/DetailedRoute.tsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom'
 
 const DetailedRoute = () => {
+
   return (
     <div>
       <section>
         // user photo and back button 
         <Link to='matches'>
           {/* <img src=''/> */}
-          <p> <- </p>
+          <p> back </p>
         </Link>
         <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.w3schools.com%2Fhowto%2Fhowto_css_image_avatar.asp&psig=AOvVaw2RdjKO-ExUR_YPaoMiw0cK&ust=1621281826441000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOC17dj_zvACFQAAAAAdAAAAABAD' />
       </section>
@@ -24,7 +25,6 @@ const DetailedRoute = () => {
       </button>
     </div>
   )
-
 }
 
 export default DetailedRoute
