c74a4de7f106443b450ecd5f446cd14b91b386d4
Alex Thompson <ahthomps1@gmail.com>
2021-05-16 13:15:10 -0600

    Create interfaces for MatchedRoutes props

--- src/components/MatchedRoutes/MatchedRoutes.tsx
+++ src/components/MatchedRoutes/MatchedRoutes.tsx
@@ -1,5 +1,24 @@
 import React, { FC, FormEvent } from 'react';
 
+
+interface RouteData {
+  routeId: number;
+  name: string;
+  distanceFromOrigin: string;
+  distanceFromDestination: string;
+  time: string;
+  days: Days[];
+}
+
+interface Days {
+  day: string;
+}
+
+interface MatchedProps {
+  allRoutes: Array<RouteData>
+}
+
+
 const MatchedRoutes = () => {
 
   return (
