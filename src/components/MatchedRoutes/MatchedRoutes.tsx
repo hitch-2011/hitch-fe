d6dfd4f929c9d4c36150ecf85357bc82ebc61b49
Alex Thompson <ahthomps1@gmail.com>
2021-05-16 13:22:46 -0600

    Add basic functionality for mapping all route data to Route cards

--- src/components/MatchedRoutes/MatchedRoutes.tsx
+++ src/components/MatchedRoutes/MatchedRoutes.tsx
@@ -1,4 +1,4 @@
-import React, { FC, FormEvent } from 'react';
+import React, { FC } from 'react';
 
 
 interface RouteData {
@@ -19,7 +19,21 @@ interface MatchedProps {
 }
 
 
-const MatchedRoutes = () => {
+const MatchedRoutes: FC<MatchedProps> = ({ allRoutes }) => {
+
+  const routeCards = allRoutes.map(route => {
+
+    return (
+      <section className='route-card'>
+        <div>
+          <p>{route.name}</p>
+          <p>{route.time}</p>
+          <p>{route.distanceFromOrigin}</p>
+          <p>{route.distanceFromDestination}</p>
+        </div>
+      </section>
+    )
+  })
 
   return (
     <h1>hello</h1>
