35e50ed48167c77dce78e4e3155718dad5c36eac
Steven Mancine <stevenrmancine@gmail.com>
2021-05-16 21:42:14 -0600

    Add placeholder logo

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+import logo from '../../assets/landing-logo.svg';
 
 const LandingPage = () => {
   return (
-    <div className='signup'>
-      {/* <img>LOGO</img> */}
+    <div className="signup">
+      <img className="signup__logo" src={logo} alt="Two passengers driving in a car" />
       <Link to="/register">
-        <button className='signup__btn btn'>Sign up now</button> 
+        <button className="signup__btn btn">Sign up now</button> 
       </Link>
     </div>
   )
