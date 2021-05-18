de54a350b7e2a465bf057f0b068a626c7656d52f
Paige Vannelli <paigevannelli@gmail.com>
2021-05-18 17:56:00 -0600

    Add .env file and add to gitignore

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import App from './components/App/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-require('dotenv').config();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -24,4 +23,6 @@ serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+require('dotenv').config();
+
 reportWebVitals();
