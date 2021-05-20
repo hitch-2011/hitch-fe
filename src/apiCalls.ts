0bf934b3d74cf8387795f08d67dfe33ec47e7028
Alex Thompson <ahthomps1@gmail.com>
2021-05-20 15:13:15 -0600

    Add a post for Route data in the apiCalls file, define type of data

--- src/apiCalls.ts
+++ src/apiCalls.ts
@@ -1,8 +1,31 @@
-export const postUserInfo = (userInfo) => {
+interface UserData {
+  email: string;
+  password: string;
+  fullname: string;
+  bio: string;
+}
+
+interface RouteData {
+  orgin: string;
+  destination: string;
+  time: string;
+  days: Array<string>;
+}
+
+export const postUserInfo = (userInfo: UserData) => {
   return fetch('', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(userInfo)
+  })
+    .then(response => response.json())
+}
+
+export const postRouteData = (routeInfo: RouteData) => {
+  return fetch('', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(routeInfo)
   })
     .then(response => response.json())
 }
