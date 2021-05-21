6b601352e989ade5eadbdfdfa576e31e5d3656fb
Steven Mancine <stevenrmancine@gmail.com>
2021-05-21 14:41:13 -0600

    Add error handling to days input

--- src/components/DaysAndTime/DaysAndTime.tsx
+++ src/components/DaysAndTime/DaysAndTime.tsx
@@ -1,18 +1,20 @@
 import React, {FC, Dispatch, SetStateAction} from 'react';
 import { DayPicker } from '../../interfaces/interfaces'
+import { BsExclamation } from 'react-icons/bs'
 
 interface DaysAndTimeProps {
   property: string;
-  method: Dispatch<SetStateAction<string>>
-  setDays: Dispatch<SetStateAction<DayPicker>>
-  days: DayPicker
+  method: Dispatch<SetStateAction<string>>;
+  setDays: Dispatch<SetStateAction<DayPicker>>;
+  days: DayPicker;
+  error: boolean;
 }
 
-const DaysAndTime: FC<DaysAndTimeProps> = ({property, method, setDays, days}) => {
+const DaysAndTime: FC<DaysAndTimeProps> = ({property, method, setDays, days, error}) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { id, checked } = e.target
-    setDays({...days, [id]: checked})
-  }
+    const { id, checked } = e.target;
+    setDays({...days, [id]: checked});
+  };
 
   const daysCheckBox = Object.keys(days).map(day => {
     return (
@@ -44,70 +46,10 @@ const DaysAndTime: FC<DaysAndTimeProps> = ({property, method, setDays, days}) =>
       
       <div className='days'>
         {daysCheckBox}
-        {/* <input 
-          id="sunday"
-          type="checkbox"
-          value="sunday"
-          checked={days['sunday']}
-          onChange={handleChange}
-        />
-        <label className="days__label" htmlFor="sunday">S
-        </label>
-        <input 
-          id="monday"
-          type="checkbox"
-          value="monday"
-          checked={days['monday']}
-          onChange={handleChange}
-        />
-        <label className="days__label" htmlFor="monday">M
-        </label>
-        <input 
-          id="tuesday"
-          type="checkbox"
-          value="tuesday"
-          checked={days['tuesday']}
-          onChange={handleChange}
-        />
-        <label className="days__label" htmlFor="tuesday">T
-        </label>
-        <input 
-          id="wednesday"
-          type="checkbox"
-          value="wednesday"
-          checked={days['wednesday']}
-          onChange={handleChange}
-        />
-        <label className="days__label" htmlFor="wednesday">W
-        </label>
-        <input 
-          id="thursday"
-          type="checkbox"
-          value="thursday"
-          checked={days['thursday']}
-          onChange={handleChange}
-        />
-        <label className="days__label" htmlFor="thursday">T
-        </label>
-        <input 
-          id="friday"
-          type="checkbox"
-          value="friday"
-          checked={days['friday']}
-          onChange={handleChange}
-        />
-        <label className="days__label" htmlFor="friday">F
-        </label>
-        <input 
-          id="saturday"
-          type="checkbox"
-          value="saturday"
-          checked={days['saturday']}
-          onChange={handleChange}
-        />
-        <label className="days__label" htmlFor="saturday">S
-        </label> */}
       </div>
+      {error && <p className="days__error">
+        <BsExclamation className="days__error__exclamation"/>Please pick at least one day
+        </p>}
     </div>
   )
 }
