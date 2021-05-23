6eeefa04aaf7d6edb4a5878f178e44ea86bec30f
Paige Vannelli <paigevannelli@gmail.com>
2021-05-22 21:16:07 -0600

    Pass down fetchedMatched user data to DetailedRoute display

--- src/components/DetailedRoute/DetailedRoute.tsx
+++ src/components/DetailedRoute/DetailedRoute.tsx
@@ -10,21 +10,33 @@ interface DetailedRouteProps {
   userId: string
 }
 
-const DetailedRoute: FC<DetailedRouteProps> = ({ userId }) => {
+interface RideData {
+  destination: string
+  origin: string
+  departure_time: string
+  zipcode_destination: string
+  zipcode_origin: string
+}
+interface UserData {
+  bio: string
+  fullname: string
+  ride_days: Array<string>
+  user_rides: Array<RideData>
+}
 
-  // const [name, setName] = useState('');
-  // const [bio, setBio] = useState('');
-  // const [time, setTime] = useState('');
-  // const [day, setDays] = useState('');
-  // const [origin, setOrigin] = useState('');
-  // const [destination, setDestination] = useState('');
+const DetailedRoute: FC<DetailedRouteProps> = ({ userId }) => {
+  
+  const [matchedUser, setMatchedUser] = useState<UserData>()
 
   useEffect(() => {
     getUserByID(parseInt(userId))
-      .then(response => console.log(response))
+      .then(response => {
+        console.log(response)
+        setMatchedUser(response.data.attributes)
+      })
   }, [userId])
 
-  const days = detailedRouteData.route.days.map((day, index) => {
+  const days = matchedUser?.ride_days.map((day, index) => {
     return (
       <div className='day' key={index}>
         {(day === 'tuesday' || day === 'thursday') ? day.charAt(0).toUpperCase() + day.slice(1,2) : day.substring(0, 1).toUpperCase()}
@@ -43,11 +55,11 @@ const DetailedRoute: FC<DetailedRouteProps> = ({ userId }) => {
       <section className='user-details'>
         <article className='user-details__driver-bio'>
           <h3>Driver Details</h3>
-          <p>{detailedRouteData.user.bio}</p>
+          <p>{matchedUser?.bio}</p>
         </article>
         <article className='user-details__route-details'>
           <h3>Time: </h3>
-          <p>{detailedRouteData.route.time}</p>
+          <p>{matchedUser?.user_rides[0].departure_time}</p>
           <h3>Days: </h3>
           <div className='user-details__days'>{days}</div>
         </article>
@@ -55,20 +67,20 @@ const DetailedRoute: FC<DetailedRouteProps> = ({ userId }) => {
       <section>
         <section className='route-details'>
           <div className='route-details__map-div'>
-            <MapDisplay />
+            <MapDisplay address={matchedUser?.user_rides[0].origin}/>
           </div>
           <div className='route-details__distance'>
-            <h3>Distance from  Origin: </h3>
-            <p className='route-details__miles'>{detailedRouteData.route.distanceFromOrigin}</p>
+            <h3>Origin Zip: </h3>
+            <p className='route-details__miles'>{matchedUser?.user_rides[0].zipcode_origin}</p>
           </div>
         </section>
         <section className='route-details'>
           <div className='route-details__map-div'>
-            <MapDisplay />
+            <MapDisplay address={matchedUser?.user_rides[0].destination}/>
           </div>
           <div className='route-details__distance'>
-            <h3>Distance from Destination: </h3>
-            <p className='route-details__miles'>{detailedRouteData.route.distanceFromDestination}</p>
+            <h3>Destination Zip: </h3>
+            <p className='route-details__miles'>{matchedUser?.user_rides[0].zipcode_destination}</p>
           </div>
         </section>
       </section>
