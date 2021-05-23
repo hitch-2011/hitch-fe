b3015591f610e499f516335bf382d4dca42500ea
Paige Vannelli <paigevannelli@gmail.com>
2021-05-22 22:07:21 -0600

    Set map state with passed down lat long

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,7 +1,31 @@
 import { Map, GoogleApiWrapper } from 'google-maps-react'
+import { useState, useEffect } from 'react';
+import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 require('dotenv').config()
 
-export const MapDisplay = (props: any) => {
+interface MapDisplayProps {
+  address: any
+  google: any
+}
+
+export const MapDisplay = ({ address, google }: MapDisplayProps) => {
+
+  const [lat, setLat] = useState(0)
+  const [long, setLong] = useState(0)
+  // const [latLong, setLatLong] = useState({lat: 0, lng: 0})
+
+  useEffect(() => {
+    if (address) {
+      geocodeByAddress(address)
+        .then(results => getLatLng(results[0]))
+        .then(latLng => {
+          console.log(latLng)
+          setLat(latLng.lat)
+          setLong(latLng.lng)
+        })
+        .catch(error => console.error('Error', error));
+    }
+  }, [address])
 
   const mapStyles = {
     width: '50%',
@@ -10,12 +34,13 @@ export const MapDisplay = (props: any) => {
 
   return (
     <Map
-    google={props.google}
+    google={google}
     style={mapStyles}
-    initialCenter={{ lat: 47.444, lng: -122.176}}
+    // initialCenter={latLong}
+    initialCenter={{lat: lat, lng: long}}
     />
-    );
-  }
+  );
+}
 
 export default GoogleApiWrapper({
   apiKey: `${process.env.REACT_APP_PLACES}`
