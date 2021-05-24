ded9ef73ead81a2dea4e5c6204c700b9a1e897b1
Paige Vannelli <paigevannelli@gmail.com>
2021-05-24 15:16:42 -0600

    Fix profile ficture data structure

--- cypress/integration/hitch-detailed-route_spec.js
+++ cypress/integration/hitch-detailed-route_spec.js
@@ -1,9 +1,53 @@
 describe('Sign up user flow', () => {
 
   beforeEach(() => {
-    cy.intercept('http://localhost:3000/profile', {fixture: 'profile'})
-    cy.visit('http://localhost:3000/profile');
+    cy.intercept('https://afternoon-journey-49986.herokuapp.com/api/v1/users', {
+      method: "POST",
+      body: 'good'
+
     })
+    cy.intercept('https://afternoon-journey-49986.herokuapp.com/api/v1/users/1/rides', {
+      method: "POST",
+      body: {
+        user_id: '1',
+        origin: '1138 Corona St, Denver, CO 80218, USA',
+        destination: '1850 Table Mesa Dr, Boulder, CO 80305',
+        departure_time: "09:03",
+        days: ["monday", "tuesday"]
+      }
+    })
+    cy.intercept('https://afternoon-journey-49986.herokuapp.com/api/v1/users/1/rides', {
+      method: "GET",
+      statusCode: 200,
+      body: {
+        data: {
+          attributes:{
+            bio: "I like driving.",
+            email: "dominic@gmail.com",
+            fullname: "fullname",
+            matched_routes: [{id: 2, user_id: 3, origin: "3956 Alcott St Denver, CO 80211, USA", destination: "1125 S Kalispell St, Aurora, CO 80017, USA", departure_time: "9:00am"}]
+          }
+        }
+      }
+    })
+    cy.intercept('https://afternoon-journey-49986.herokuapp.com/api/v1/users/1', {fixture: 'profile'})
+
+    cy.visit('http://localhost:3000/profile');
+  })
+
+    it('should have an app title', () => {
+      cy.get('h1').contains('HITCH');
+    });
+
+})
+
+  // before(() => {
+  //   cy.visit('http://localhost:3000');
+  //   cy.get('[data-cy=login-button]')
+  //   .click()
+  //   .intercept('http://localhost:3000/profile', {fixture: 'profile'})
+  //   cy.request('http://localhost:3000/profile');
+  //   })
 
   // it('should have an app title', () => {
   //   cy.get('h1').contains('HITCH');
@@ -17,4 +61,4 @@ describe('Sign up user flow', () => {
   //   cy.get('button').eq(0).click();
   //   cy.url().should('include', 'http://localhost:3000/register')
   // });
-});
+// });
