3069cc3f7a1c6ddd5a739ec88de421df38d820d0
Paige Vannelli <paigevannelli@gmail.com>
2021-05-25 11:35:49 -0600

    Delete commented out code

--- cypress/integration/hitch-detailed-route_spec.js
+++ cypress/integration/hitch-detailed-route_spec.js
@@ -43,13 +43,6 @@ describe('Sign up user flow', () => {
     })
   })
 
-  it('Should allow the user to click on the menu', () => {
-    cy.get('[data-cy=menu]')
-    .click()
-    .get('[data-cy=profile-button]')
-    .should('exist')
-  });
-
   it('Should display a header', () => {
     cy.get('[data-cy=hitch-header-title]')
     .should('exist')
@@ -84,26 +77,11 @@ describe('Sign up user flow', () => {
     cy.get('[data-cy=add-route-button]')
     .contains("Add a Route")
   });
+
+  it('Should allow the user to click on the menu', () => {
+    cy.get('[data-cy=menu]')
+    .click()
+    .get('[data-cy=profile-button]')
+    .should('exist')
+  });
 })
-
-  // before(() => {
-  //   cy.visit('http://localhost:3000');
-  //   cy.get('[data-cy=login-button]')
-  //   .click()
-  //   .intercept('http://localhost:3000/profile', {fixture: 'profile'})
-  //   cy.request('http://localhost:3000/profile');
-  //   })
-
-  // it('should have an app title', () => {
-  //   cy.get('h1').contains('HITCH');
-  // });
-
-  // it('should have button to sign up', () => {
-  //   cy.get('button').contains('Sign up');
-  // });
-
-  // it('should be able to click \'sign up now\' button', () => {
-  //   cy.get('button').eq(0).click();
-  //   cy.url().should('include', 'http://localhost:3000/register')
-  // });
-// });
