38a4d8d93748f1f4a8e6408fdcee81d23c4522e6
Paige Vannelli <paigevannelli@gmail.com>
2021-05-26 09:18:39 -0600

    Remove unused tests

--- cypress/integration/hitch-menu_spec.js
+++ cypress/integration/hitch-menu_spec.js
@@ -189,19 +189,3 @@ describe('The clickable links on the menu', () => {
     cy.get('.profile-link').click()
       .url().should('include', '/profile');
   });
-
-  it('Should be able to travel to requests', () => {
-    cy.get('.menu-link').eq(0).click()
-      .url().should('include', '/requests');
-  });
-
-  it('Should be able to travel to matched routes', () => {
-    cy.get('.menu-link').eq(1).click()
-      .url().should('include', '/matched-routes');
-  });
-
-  it('Should be able to travel to pending-routes', () => {
-    cy.get('.menu-link').eq(2).click()
-      .url().should('include', '/pending-routes');
-  });
-})
